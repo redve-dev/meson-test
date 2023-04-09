3bf3f5cc4372a8fbf618bc35d15c9ec718813fca
Beniamin <106469071+redve-dev@users.noreply.github.com>
2023-04-09 16:57:44 +0200

    isolate enum class

--- tests/logger.cpp
+++ tests/logger.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <functional>
 
-int TestLogger(bool exoected_to_print, const Logger::LogLevel logger_level, Logger::LogLevel message_level){
+int TestLogger(bool exoected_to_print, const LogLevel logger_level, LogLevel message_level){
 	static int test_no = 0;
 	std::cout << "Test logger no: " << ++test_no << '\n';
 	std::flush(std::cout);
@@ -24,15 +24,14 @@ int TestLogger(bool exoected_to_print, const Logger::LogLevel logger_level, Logg
 	std::cout << output;
 
 	std::cout << "End test logger no: " << test_no << "\n==========================\n\n";
-	return exoected_to_print != output.empty();
-}
+	return exoected_to_print != output.empty(); }
 
 int test_dumping_output(Logger& logger, size_t expected_lines){
-	logger.Log("Test info", Logger::LogLevel::INFO);
-	logger.Log("Test warning", Logger::LogLevel::WARNING);
-	logger.Log("Test error", Logger::LogLevel::ERROR);
-	logger.Log("Test debug", Logger::LogLevel::DEBUG);
-	logger.Log("Test empty", Logger::LogLevel::EMPTY);
+	logger.Log("Test info", LogLevel::INFO);
+	logger.Log("Test warning", LogLevel::WARNING);
+	logger.Log("Test error", LogLevel::ERROR);
+	logger.Log("Test debug", LogLevel::DEBUG);
+	logger.Log("Test empty", LogLevel::EMPTY);
 	logger.DumpLogsToFile("test.txt");
 	std::ifstream file("test.txt");
 	std::vector<std::string> lines;
@@ -48,25 +47,26 @@ int test_dumping_output(Logger& logger, size_t expected_lines){
 int tests_logger_dumping_output(){
 	int error = 0;
 	Logger logger;
-	auto function_cycle = std::function<void(Logger::LogLevel, int)>([&](Logger::LogLevel level, int expected){
+	auto function_cycle = std::function<void(LogLevel, int)>([&](LogLevel level, int expected){
 		logger.SetLogLevel(level);
 		error += test_dumping_output(logger, expected);
 		logger.ClearLogs();
 	});
 
-	function_cycle(Logger::LogLevel::INFO, 4);
-	function_cycle(Logger::LogLevel::WARNING, 3);
-	function_cycle(Logger::LogLevel::ERROR, 2);
-	function_cycle(Logger::LogLevel::DEBUG, 1);
-	function_cycle(Logger::LogLevel::EMPTY, 0);
+	function_cycle(LogLevel::INFO, 4);
+	function_cycle(LogLevel::WARNING, 3);
+	function_cycle(LogLevel::ERROR, 2);
+	function_cycle(LogLevel::DEBUG, 1);
+	function_cycle(LogLevel::EMPTY, 0);
 	return error;
 }
 
 
+
 int main(){
 	int error = 0;
-	//error += TestLogger(true, Logger::LogLevel::DEBUG, Logger::LogLevel::DEBUG); // fails
-	//error += TestLogger(false, Logger::LogLevel::EMPTY, Logger::LogLevel::DEBUG);
+	//error += TestLogger(true, LogLevel::DEBUG, LogLevel::DEBUG); // fails
+	//error += TestLogger(false, LogLevel::EMPTY, LogLevel::DEBUG);
 	//error += tests_logger_dumping_output();
 	return error;
 }
