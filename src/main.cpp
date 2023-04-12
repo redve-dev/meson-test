1f7afbd669992c2739dce96396f3df69873779f5
Beniamin <106469071+redve-dev@users.noreply.github.com>
2023-04-12 17:33:13 +0200

    change main loop a little

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 #include "include/Game.hpp"
 
 int main(){
+	try{
 	Game game(800, 600);
 	game.MainLoop();
+	}
+	catch (std::exception& e){
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
